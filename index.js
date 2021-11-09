e4a9cb6656293060131c97b22247f500f611a083
Carlton Lauckern <reed@Carltons-MacBook-Air.local>
2021-11-09 12:35:52 -0800

    Finished-all-tasks

--- index.js
+++ index.js
@@ -74,10 +74,13 @@ Use the higher-order function getWinnersByYear to do the following:
 hint: the strings returned need to exactly match the string in step 4.
  */
 
-function getWinnersByYear(/* code here */) {
-    /* code here */
+function getWinnersByYear(data, getYearsCB, getWinnersCB ) {
+    const winners = getWinnersCB(data, getFinals);
+    const years = getYearsCB(data, getFinals);
+    return winners.map((item, index)=> `In ${years[index]}, ${item} won the world cup!`);
 }
 
+console.log(getWinnersByYear(fifaData, getYears, getWinners));
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 6: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
@@ -90,10 +93,14 @@ Use the higher order function getAverageGoals to do the following:
  Example of invocation: getAverageGoals(getFinals(fifaData));
 */
 
-function getAverageGoals(/* code here */) {
-   /* code here */
-}
-
+function getAverageGoals(data) {
+    const averageHomeGoals = data.reduce(function(acc, item) {
+        return acc + item['Home Team Goals'] + item['Away Team Goals'];
+ 
+    }, 0)
+    return (averageHomeGoals / data.length).toFixed(2);
+ }
+ console.log('Task 6:', getAverageGoals(fifaData));
 
 
 
